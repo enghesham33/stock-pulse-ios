6f76a660c886b2223d4674a5dd23eea1baf39a45
Hesham Khaled <heshamkhaled92@gmail.com>
2026-03-26 20:51:43 +0200

    Splash screen
    The first view that will appear to the user before the listing screen

---
+++ StockPulse/Features/Splash/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  StockPulse
+//
+//  Created by Hesham Khaled on 25/03/2026.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        ZStack {
+            Text("StockPulse App")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                
+        }
+        .ignoresSafeArea()
+    }
+}
